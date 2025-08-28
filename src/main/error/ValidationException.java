9eff8a13648f56c638504468ef0515ff63f1c3eb
MVCx9 <miguelvdezc@gmail.com>
2025-08-28 20:29:11 +0200

    feat(errors): specific errors

---
+++ src/main/error/ValidationException.java
@@ -0,0 +1,15 @@
+package main.error;
+
+public class ValidationException extends PluginException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ValidationException(String message) {
+		super(message);
+	}
+
+	public ValidationException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
