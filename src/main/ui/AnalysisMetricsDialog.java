8806f75f3221054efee107f75e4e83f2bc7b141c
MVCx9 <miguelvdezc@gmail.com>
2025-09-03 02:51:30 +0200

    refactor(imports): moved classes

--- src/main/ui/AnalysisMetricsDialog.java
+++ src/main/ui/AnalysisMetricsDialog.java
@@ -6,14 +6,14 @@ import org.eclipse.jface.dialogs.TitleAreaDialog;
 import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.SashForm;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.custom.StyledText;
-import org.eclipse.swt.graphics.Font;
 
 import main.model.clazz.ClassMetrics;
 import main.model.common.ComplexityStats;
