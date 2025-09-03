057ee59a7082616951bc679e25f5579767bdaf57
MVCx9 <miguelvdezc@gmail.com>
2025-09-03 18:55:53 +0200

    Remove Utils and related test and mother classes
    Deleted src/main/refactor/Utils.java and all associated test files and test data mother classes. This removes utility methods and their tests, as well as supporting test infrastructure, likely as part of a refactor or cleanup.

--- src/test/util/AssertionsStrict.java
+++
@@ -1,9 +0,0 @@
-package test.util;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-public final class AssertionsStrict {
-    private AssertionsStrict() {}
-    public static void toStrictEqual(Object expected, Object actual) { assertEquals(expected, actual); }
-    public static void toStrictEqual(int expected, int actual) { assertEquals(expected, actual); }
-}
