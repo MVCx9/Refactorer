daed9520b33facd9c7e5b0b83f6d00f0735ddef2
MVCx9 <miguelvdezc@gmail.com>
2025-09-03 02:49:08 +0200

    refactor(imports): refactor en test NEO

--- src/test/neo/algorithms/SequenceTest.java
+++ src/test/neo/algorithms/SequenceTest.java
@@ -1,6 +1,6 @@
 package test.neo.algorithms;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.File;
 import java.nio.file.Path;
