aca7a7961be5bca6e07d98db05dc07d773a51e66
MVCx9 <miguelvdezc@gmail.com>
2025-09-11 03:52:24 +0200

    Add comprehensive tests for MethodAnalysisMetricsMapper
    Expanded MethodAnalysisMetricsMapperTest with multiple test cases covering mapping from MethodAnalysis and RefactorComparison to MethodMetrics and MethodAnalysis, including null and edge cases. Added utility methods and improved test coverage. Also added mother classes for MethodAnalysis and MethodMetrics to facilitate test data creation.

--- src/test/model/method/mother/MethodAnalysisMother.java
+++ src/test/model/method/mother/MethodAnalysisMother.java
@@ -1,5 +1,83 @@
 package test.model.method.mother;
 
-public class MethodAnalysisMother {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.ltk.core.refactoring.Change;
+
+import main.builder.MethodAnalysis;
+import main.neo.cem.CodeExtractionMetrics;
+import main.neo.cem.CodeExtractionMetricsStats;
+
+public final class MethodAnalysisMother {
+	private static final String DEFAULT_METHOD_NAME = "method";
+	private static final int DEFAULT_CC = 7;
+	private static final int DEFAULT_LOC = 20;
+	private static final int DEFAULT_REDUCED_COMPLEXITY = 3;
+	private static final int DEFAULT_NUMBER_OF_EXTRACTIONS = 2;
+
+	private MethodAnalysisMother() {}
+
+	public static MethodAnalysis defaultAnalysis() {
+		final CompilationUnit cu = sampleCompilationUnit();
+		final CodeExtractionMetricsStats stats = sampleStats();
+		return MethodAnalysis.builder()
+				.methodName(DEFAULT_METHOD_NAME)
+				.cc(DEFAULT_CC)
+				.loc(DEFAULT_LOC)
+				.reducedComplexity(DEFAULT_REDUCED_COMPLEXITY)
+				.numberOfExtractions(DEFAULT_NUMBER_OF_EXTRACTIONS)
+				.compilationUnitRefactored(cu)
+				.stats(stats)
+				.build();
+	}
+
+	public static MethodAnalysis withMethodName(String name) {
+		final String n = name == null ? DEFAULT_METHOD_NAME : name;
+		final CompilationUnit cu = sampleCompilationUnit();
+		final CodeExtractionMetricsStats stats = sampleStats();
+		return MethodAnalysis.builder()
+				.methodName(n)
+				.cc(DEFAULT_CC)
+				.loc(DEFAULT_LOC)
+				.reducedComplexity(DEFAULT_REDUCED_COMPLEXITY)
+				.numberOfExtractions(DEFAULT_NUMBER_OF_EXTRACTIONS)
+				.compilationUnitRefactored(cu)
+				.stats(stats)
+				.build();
+	}
+
+	public static MethodAnalysis custom(String methodName, int cc, int loc, int reducedComplexity, int numberOfExtractions, CompilationUnit cu, CodeExtractionMetricsStats stats) {
+		final String n = methodName == null ? DEFAULT_METHOD_NAME : methodName;
+		final CompilationUnit compilationUnit = cu == null ? sampleCompilationUnit() : cu;
+		final CodeExtractionMetricsStats s = stats == null ? sampleStats() : stats;
+		return MethodAnalysis.builder()
+				.methodName(n)
+				.cc(cc)
+				.loc(loc)
+				.reducedComplexity(reducedComplexity)
+				.numberOfExtractions(numberOfExtractions)
+				.compilationUnitRefactored(compilationUnit)
+				.stats(s)
+				.build();
+	}
+
+	private static CompilationUnit sampleCompilationUnit() {
+		final ASTParser parser = ASTParser.newParser(AST.getJLSLatest());
+		final String src = "public class Sample { void x() { int a=0; } }";
+		parser.setKind(ASTParser.K_COMPILATION_UNIT);
+		parser.setSource(src.toCharArray());
+		parser.setResolveBindings(false);
+		return (CompilationUnit) parser.createAST(null);
+	}
+
+	private static CodeExtractionMetricsStats sampleStats() {
+		final List<Change> empty = new ArrayList<>();
+		final CodeExtractionMetrics m1 = new CodeExtractionMetrics(true, "OK", true, 5, 1, empty, empty);
+		final CodeExtractionMetrics m2 = new CodeExtractionMetrics(true, "OK", true, 7, 2, empty, empty);
+		return new CodeExtractionMetricsStats(new CodeExtractionMetrics[] { m1, m2 });
+	}
 }
--- src/test/model/method/mother/MethodMetricsMother.java
+++ src/test/model/method/mother/MethodMetricsMother.java
@@ -1,5 +1,37 @@
 package test.model.method.mother;
 
-public class MethodMetricsMother {
+import main.model.method.MethodMetrics;
 
+public final class MethodMetricsMother {
+	private static final String DEFAULT_NAME = "method";
+	private static final int DEFAULT_LOC = 10;
+	private static final int DEFAULT_CC = 5;
+
+	private MethodMetricsMother() {}
+
+	public static MethodMetrics defaultMetrics() {
+		return MethodMetrics.builder()
+				.name(DEFAULT_NAME)
+				.loc(DEFAULT_LOC)
+				.cc(DEFAULT_CC)
+				.build();
+	}
+
+	public static MethodMetrics withName(String name) {
+		final String n = name == null ? DEFAULT_NAME : name;
+		return MethodMetrics.builder()
+				.name(n)
+				.loc(DEFAULT_LOC)
+				.cc(DEFAULT_CC)
+				.build();
+	}
+
+	public static MethodMetrics custom(String name, int loc, int cc) {
+		final String n = name == null ? DEFAULT_NAME : name;
+		return MethodMetrics.builder()
+				.name(n)
+				.loc(loc)
+				.cc(cc)
+				.build();
+	}
 }
